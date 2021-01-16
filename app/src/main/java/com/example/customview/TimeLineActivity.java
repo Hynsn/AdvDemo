910f30b78a782668fcab22762fed6e2290ebfb5b
hynson <hynson@163.com>
2021-01-16 15:45:49 +0800

    自定义控件NumberText

--- app/src/main/java/com/example/customview/TimeLineActivity.java
+++ app/src/main/java/com/example/customview/TimeLineActivity.java
@@ -1,8 +1,10 @@
 package com.example.customview;
 
+import android.annotation.SuppressLint;
+import android.text.Editable;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.R;
 import com.example.base.BaseActivity;
@@ -13,8 +15,6 @@ import java.util.List;
 
 public class TimeLineActivity extends BaseActivity<ActivityTimelineBinding> {
     final static String TAG = TimeLineActivity.class.getSimpleName();
-    private TimeProgressBar timePB;
-    private Button nextBtn,resetBtn;
     private int progress = 0;
     List<Integer> stepSeconds;
 
@@ -23,6 +23,7 @@ public class TimeLineActivity extends BaseActivity<ActivityTimelineBinding> {
         return R.layout.activity_timeline;
     }
 
+    @SuppressLint("RtlHardcoded")
     @Override
     protected void bindView() {
         stepSeconds = new ArrayList<>();
@@ -41,9 +42,30 @@ public class TimeLineActivity extends BaseActivity<ActivityTimelineBinding> {
         stepSeconds.add(4);
 
         binding.timePb.setStep(9,stepSeconds);
+        binding.tvNumber.setGravity(Gravity.RIGHT);
     }
 
     public void click(View v){
         Log.i(TAG, "click: ");
+        switch (v.getId()){
+            case R.id.btn_reset:
+                progress = 0;
+                binding.timePb.setCurrentPos(progress);
+                break;
+            case R.id.btn_next:
+                if(progress>20) {
+                    binding.timePb.setCurrentPos(progress);
+                    progress++;
+                }
+                break;
+            case R.id.btn_del:
+                Editable editable = binding.tvNumber.getText();
+                binding.tvNumber.setSelection(editable.length());
+                int start = binding.tvNumber.getSelectionStart();
+                if (start > 0) {
+                    editable.delete(start - 1, start);
+                }
+                break;
+        }
     }
 }

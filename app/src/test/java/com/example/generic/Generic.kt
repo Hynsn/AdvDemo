796bb44ae14f20b7e636fdfc031179a471fb6bf9
hynson <hynson@163.com>
2021-07-21 22:17:31 +0800

    泛型结合kt构造函数使用

---
+++ app/src/test/java/com/example/generic/Generic.kt
@@ -0,0 +1,47 @@
+package com.example.generic
+
+data class Generic<T>(
+    var a:Int,
+    var b:Int,
+    var diff:T?
+)
+
+data class AirFryDiff(
+    // 无法解决同类型参数问题，如果是这种情况可以考虑将两个参数复用为同一个，如x,y -> xy
+    var a:Int?,
+    var b: String?
+){
+    constructor(b:Int?) : this(0,null)
+    constructor(a:String?) : this(0,a)
+}
+
+// 主要函数
+data class P(
+    var name: String?,
+    var age:Int
+){
+    //次要构造函数
+    constructor(int: Int) : this(null,int)
+    constructor(s: String?) : this(s,0)
+}
+
+class Person {
+    var name:String? = null
+    var age:Int = 0
+    var sex:String? = null
+
+    constructor(name: String) {
+        this.name = name
+    }
+    constructor(sex: String,name: String):this(name){
+        this.sex = sex
+        this.name = name
+    }
+    constructor(age: Int){
+        this.age = age
+    }
+
+    override fun toString(): String {
+        return "$name , $age , $sex"
+    }
+}

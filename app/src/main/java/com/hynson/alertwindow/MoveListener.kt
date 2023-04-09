0a54c509ef7351e80d8592d2f0175dfc0c2996de
Hynsn <hynson@163.com>
2023-04-09 22:35:17 +0800

    增加吸附动画

--- app/src/main/java/com/hynson/alertwindow/MoveListener.kt
+++ app/src/main/java/com/hynson/alertwindow/MoveListener.kt
@@ -1,6 +1,7 @@
 package com.hynson.alertwindow
 
 import android.animation.ValueAnimator
+import android.graphics.Rect
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
@@ -15,6 +16,7 @@ class MoveListener(val params: WindowManager.LayoutParams, val windowManager: Wi
     // 手指按下的坐标
     private var mViewDownX = 0f
     private var mViewDownY = 0f
+    private var outRect = Rect()
 
     override fun onTouch(view: View, event: MotionEvent): Boolean {
         when (event.action) {
@@ -24,6 +26,8 @@ class MoveListener(val params: WindowManager.LayoutParams, val windowManager: Wi
                 // 记录按下的位置（相对 View 的坐标）
                 mViewDownX = event.x
                 mViewDownY = event.y
+
+                view.getWindowVisibleDisplayFrame(outRect)
             }
 
             MotionEvent.ACTION_MOVE -> {
@@ -41,14 +45,14 @@ class MoveListener(val params: WindowManager.LayoutParams, val windowManager: Wi
                 windowManager.updateViewLayout(view, params)
             }
 
+            MotionEvent.ACTION_CANCEL,
             MotionEvent.ACTION_UP -> {
                 // 记录移动的位置（相对屏幕的坐标）
                 val rawMoveX = event.rawX - getWindowInvisibleWidth()
                 val rawMoveY = event.rawY - getWindowInvisibleHeight()
                 val rawFinalX: Float
                 // 获取当前屏幕的宽度
-                // 获取当前屏幕的宽度
-                val screenWidth: Int = windowManager.defaultDisplay.width
+                val screenWidth: Int = getWindowWidth()
                 rawFinalX = if (rawMoveX < screenWidth / 2f) {
                     // 回弹到屏幕左边
                     0f
@@ -57,8 +61,8 @@ class MoveListener(val params: WindowManager.LayoutParams, val windowManager: Wi
                     screenWidth.toFloat()
                 }
                 // 从移动的点回弹到边界上
-                // 从移动的点回弹到边界上
                 startHorizontalAnimation(
+                    view,
                     rawMoveX - mViewDownX,
                     rawFinalX - mViewDownX, rawMoveY - mViewDownY
                 )
@@ -72,27 +76,32 @@ class MoveListener(val params: WindowManager.LayoutParams, val windowManager: Wi
         return false
     }
 
+    private fun getWindowWidth(): Int {
+        return outRect.right - outRect.left
+    }
+
     private fun getWindowInvisibleHeight(): Int {
-        return 0
+        return outRect.top
     }
 
     private fun getWindowInvisibleWidth(): Int {
-        return 0
+        return outRect.left
     }
 
-    private fun startHorizontalAnimation(startX: Float, endX: Float, y: Float) {
+    private fun startHorizontalAnimation(view: View, startX: Float, endX: Float, y: Float) {
         val animator = ValueAnimator.ofFloat(startX, endX)
         animator.duration = calculateAnimationDuration(startX, endX)
         animator.addUpdateListener { animation: ValueAnimator ->
             updateLocation(
-                animation.animatedValue as Int,
+                view,
+                (animation.animatedValue as Float).toInt(),
                 y.toInt()
             )
         }
         animator.start()
     }
 
-    private fun updateLocation(x: Int, y: Int) {
+    private fun updateLocation(view: View, x: Int, y: Int) {
         // 屏幕默认的重心（一定要先设置重心位置为左上角）
         val screenGravity = Gravity.TOP or Gravity.START
         val params: WindowManager.LayoutParams = params
@@ -103,7 +112,7 @@ class MoveListener(val params: WindowManager.LayoutParams, val windowManager: Wi
         params.x = x
         params.y = y
         params.gravity = screenGravity
-        //        windowManager.update()
+        windowManager.updateViewLayout(view, params)
     }
 
     private fun calculateAnimationDuration(startCoordinate: Float, endCoordinate: Float): Long {
